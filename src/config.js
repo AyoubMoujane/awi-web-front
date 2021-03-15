d7719084159f800dad12b3be37f0b6a30baf9200
Pierre34170 <perr1.pierre34@gmail.com>
2021-03-16 00:07:37 +0100

    change url_api

--- src/config.js
+++ src/config.js
@@ -2,17 +2,20 @@ const Config = function(){
     switch(process.env.NODE_ENV){
         case 'development':
             return {
-                API_URL : "http://localhost:8080/api/auth/"
+                API_AUTH_URL : "http://localhost:8080/api/auth/",
+                API_APP_URL : "http://localhost:8080/api/app/"
             };
 
         case 'production':
             return {
-                API_URL : "http://15.236.140.17:8080/api/auth/"
+                API_AUTH_URL : "http://15.236.140.17:8080/api/auth/",
+                API_APP_URL : "http://15.236.140.17:8080/api/app/"
             };
 
         default:
             return {
-                API_URL : "http://localhost:8080/api/auth/"
+                API_AUTH_URL : "http://localhost:8080/api/auth/",
+                API_APP_URL : "http://localhost:8080/api/app/"
             };
     }
 };
--- src/services/user/user.js
+++ src/services/user/user.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from '../authentification/auth-header';
 const config = require('../../config')
 
-const API_URL = config.API_URL
+const API_URL = config.API_APP_URL
 
 class UserService {
     getPublicContent() {
