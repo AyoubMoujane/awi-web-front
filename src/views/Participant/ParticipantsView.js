6b61399b5a2c1b9bb8f3651e83e1e599c0741c2c
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-03-19 09:01:59 +0100

    Implemented rendering of every participant with fake data

--- src/views/Participant/ParticipantsView.js
+++ src/views/Participant/ParticipantsView.js
@@ -7,9 +7,32 @@ export function ParticipantsView() {
     const [errors, setErrors] = useState(null)
     const [loading, setLoading] = useState(false)
 
+    const fakeParticipants = [
+        {
+            idParticipant: 1,
+            nomParticipant: "Ankama",
+            editeurSeulement: 0
+        }, {
+            idParticipant: 2,
+            nomParticipant: "Asmodee",
+            editeurSeulement: 0
+        }, {
+            idParticipant: 3,
+            nomParticipant: "Gigamic",
+            editeurSeulement: 1
+        }
+    ]
+
+
     return (
         <div>
-            <ParticipantItem />
+            <p>Participants</p>
+
+            {fakeParticipants.map(function (item, i) {
+                return <ParticipantItem key={i} data={item} />
+            })}
+
+
         </div>
     )
 }
