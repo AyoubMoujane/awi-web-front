fcaf59c15bf5da631781567d30bb9685cb6d9f5c
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-03-31 22:10:45 +0200

    Implemented fetching profile after login

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
 import AuthService from "../../services/authentification/auth";
 
+import { useDispatch } from 'react-redux'
+import { fetchFestivals } from "../../redux/actions/festival/festivalActions"
+
 export function Profile() {
 
+    const dispatch = useDispatch()
+    useEffect(() => dispatch(fetchFestivals()), [])
 
     const currentUser = AuthService.getCurrentUser()
 
